90919caf6920248200f144133f145a8c5df42e14
steeven-louk <steevenlouk.sl@gmail.com>
2022-08-19 23:35:42 +0200

    finition du banner

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Header } from '../../components/header/Header'
 
+import './styles/style.scss'
+
 export const Home = () => {
   return (
     <>
         <div className="header">
             <Header />
         </div>
+        <section className="heros">
+            <button><span>découvrez-le</span></button>
+        </section>
     </>
   )
 }
