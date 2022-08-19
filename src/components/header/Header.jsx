e18f34797d29d4a130012856667ae07ee3e71006
steeven-louk <steevenlouk.sl@gmail.com>
2022-08-19 22:39:58 +0200

    finition de la navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,17 +9,30 @@ export const Header = () => {
         <div className="menu">
             <img src="./assets/menu.png" alt="" className="menu_icons" />
             <ul>
-            <li>notre carte</li>
-            <li>le coin famille</li>
-            <li>recrutez-nous!</li>
-            <li>burger kara</li>
+            <li><a href="/">notre carte</a> </li>
+            <li><a href="/">le coin famille</a> </li>
+            <li><a href="/">recrutez-nous!</a> </li>
+            <li><a href="/">burger kara</a> </li>
         </ul>
         </div>
         <img src="./assets/burger-king.png" alt="" className='icon'/>
 
         <div className="header-left">
          <FontAwesomeIcon icon="fa-solid fa-location-dot" className='icon-right' />
-         <FontAwesomeIcon icon="fa-solid fa-crown"  className='icon-right'/>
+         
+         <div className="crow">
+            <FontAwesomeIcon icon="fa-solid fa-crown"  className='icon-right' />
+
+         <div className="subMenu">
+            <button className='seConnecter'>se connecter</button>
+            <hr />
+            <p>vous n'avez pas encore de compte ?</p>
+
+            <button className='rejoindre'>rejoignez le kingdom</button>
+         </div>
+         </div>
+         
+
 
          <button><span>click & colelct</span> <img src="./assets/click_collect.png" alt="" className='btn-icon' /></button>
         </div>
