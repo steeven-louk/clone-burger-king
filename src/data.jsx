1c0a07f842434614babd9184ad43d43cc596a980
steeven-louk <steevenlouk.sl@gmail.com>
2022-08-20 01:07:50 +0200

    mise en place de facin dynamique de la card1

---
+++ src/data.jsx
@@ -0,0 +1,19 @@
+// const img2 = './assets/img2.webp'
+// const img3 = '../public/assets/img3.webp'
+
+const Chaud =[
+    {
+        'name': 'desserts glacés',
+        'img': './assets/img2.webp'
+    },
+    {
+        'name': 'bons plans !',
+        'img': './assets/img3.webp'
+    },
+    {
+        'name': 'en ce moment',
+        'img': './assets/img4.webp'
+    },
+]
+
+export default Chaud;
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Card1 from '../../components/card/Card1'
 import { Header } from '../../components/header/Header'
+import Chaud from '../../data'
 
 import './styles/style.scss'
 
 export const Home = () => {
+
+ const [chaudSection] = useState(Chaud);
+
+//    console.log('chaud', chaudSection);
+
   return (
     <>
         <div className="header">
@@ -11,6 +18,22 @@ export const Home = () => {
         </div>
         <section className="heros">
             <button><span>découvrez-le</span></button>
+        </section>
+
+        <section className="chaudBouillant">
+            <div className="title">
+                <h1>chaud bouillant !</h1>
+                <p>Découvrez les actualités Burger King®</p>
+            </div>
+
+            <div className="chaudBouillant__card">
+                {chaudSection?.map((item, index) =>(
+                     <Card1 item={item} index={index}/>
+            ))} 
+            </div>
+            
+            
+           
         </section>
     </>
   )
